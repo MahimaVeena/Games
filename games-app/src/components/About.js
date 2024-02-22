b6bb6e593c61e479d3ba252e06a6e9f0631834da
Mahima <mahima21389@gmail.com>
2024-02-22 09:49:30 +0530

    5

--- games-app/src/components/About.js
+++ games-app/src/components/About.js
@@ -5,22 +5,25 @@ import Footer from "./Footer";
 function About() {
   return (
     <>
-      <div className="about">
-        <p className="first">
-          Hello there! I'm Mahima, hailing from the vibrant state of Karnataka.
-          My journey in the tech world began after completing my engineering
-          degree in Electronics and Communication Engineering (ECE). I am a back
-          end developer with 3+ years of experience. I have joined Wissen on 5th
-          Feb 2024.
-        </p>
-        <br />
-        <p className="second">
-          Recently, I embarked on an exciting project to build a website from
-          the ground up. This endeavor was not just a test of my coding
-          abilities but also a significant leap towards enhancing my front-end
-          development skills. Thank you for taking the time to learn about my
-          journey.
-        </p>
+      <div className="main">
+        <img src="/images/woman.webp" />
+        <div className="about">
+          <p className="first">
+            Hello there! I'm Mahima, hailing from the vibrant state of
+            Karnataka. My journey in the tech world began after completing my
+            engineering degree in Electronics and Communication Engineering
+            (ECE). I am a back end developer with 3+ years of experience. I have
+            joined Wissen on 5th Feb 2024.
+          </p>
+          <br />
+          <p className="second">
+            Recently, I embarked on an exciting project to build a website from
+            the ground up. This endeavor was not just a test of my coding
+            abilities but also a significant leap towards enhancing my front-end
+            development skills. Thank you for taking the time to learn about my
+            journey.
+          </p>
+        </div>
       </div>
       <Footer />
     </>
